ac568d4e406826d69c0900bc939d4661aecf5a70
Steve Konves <stephen.konves@gmail.com>
2022-01-23 12:52:51 -0700

    feat: import CLI

---
+++ src/cli.ts
@@ -0,0 +1,175 @@
+#!/usr/bin/env node
+
+import { readFile, writeFile, mkdir } from 'fs/promises';
+import { watchFile } from 'fs';
+import { join, sep } from 'path';
+
+import yargs = require('yargs/yargs');
+import { hideBin } from 'yargs/helpers';
+import {
+  Config,
+  File,
+  Generator,
+  GeneratorOptions,
+  Parser,
+  Service,
+} from './types';
+
+const { argv } = yargs(hideBin(process.argv))
+  .option('config', {
+    alias: 'c',
+    string: true,
+    description: 'Path to the config file. Defaults to ility.conifg.json.',
+    default: 'ility.config.json',
+    requiresArg: true,
+  })
+  .option('source', {
+    alias: 's',
+    string: true,
+    description:
+      'Path to an SDL file. Reads from stdin if omitted and config is not found.',
+    requiresArg: true,
+  })
+  .option('output', {
+    alias: 'o',
+    string: true,
+    description:
+      'Path of the output folder. Writes to the current working directory if omitted and config is not found.',
+    requiresArg: true,
+  })
+  .option('generators', {
+    alias: 'g',
+    string: true,
+    description: `Generators`,
+    requiresArg: true,
+  })
+  .option('watch', {
+    alias: 'w',
+    boolean: true,
+    description: 'Recreates the output each time the input file changes.',
+    nargs: 0,
+  });
+
+(async () => {
+  try {
+    const { config: configPath, source, output, watch } = await argv;
+
+    const config = await getConfig(configPath);
+
+    const sdlPath = source || config.source;
+
+    if (watch && !sdlPath) {
+      throw new Error('Must specify source when running in watch mode.');
+    }
+
+    const x = output || config.output || '';
+    const outputPath = x.startsWith('/') ? x.substring(1) : x;
+
+    let sdl: string | undefined;
+
+    if (sdlPath) {
+      sdl = (await readFile(sdlPath)).toString('utf8');
+    } else if (!process.stdin.isTTY) {
+      sdl = await readStreamToString(process.stdin);
+    } else {
+      throw new Error('No input file provided and nothing to read from stdin');
+    }
+
+    if (sdl) {
+      const parser = getParser(config.parser);
+      const generators = getGenerators(config.generators);
+
+      const service = parser(sdl);
+      await doGenerate(outputPath, service, generators);
+
+      if (watch && sdlPath) {
+        watchFile(sdlPath, async () => {
+          const newSdl = (await readFile(sdlPath)).toString('utf8');
+          await doGenerate(outputPath, parser(newSdl), generators);
+        });
+      }
+    }
+  } catch (ex) {
+    console.error('Did not generate server types', ex.message);
+    process.exit(1);
+  }
+})();
+
+async function doGenerate(
+  output: string,
+  service: Service,
+  generators: Generator[],
+): Promise<void> {
+  console.log('Writing files ...');
+  for (const file of getFiles(service, generators)) {
+    const path = file.path.slice(0, file.path.length - 1);
+    const filename = file.path[file.path.length - 1];
+
+    const fullpath = [...output.split(sep), ...path];
+    const finalName = join(...fullpath, filename);
+
+    console.log(`+ ${finalName}`);
+
+    await mkdir(join(...fullpath), { recursive: true });
+    await writeFile(join(...fullpath, filename), file.contents);
+  }
+  console.log();
+}
+
+async function getConfig(path: string): Promise<Config> {
+  try {
+    return JSON.parse(
+      ((await readFile(join(process.cwd(), path))) || {}).toString(),
+    );
+  } catch {}
+
+  try {
+    return JSON.parse(
+      ((await readFile(join(__dirname, path))) || {}).toString(),
+    );
+  } catch {}
+
+  try {
+    return JSON.parse(((await readFile(path)) || {}).toString());
+  } catch {}
+
+  return {
+    parser: '',
+    generators: [],
+  };
+}
+
+function getParser(moduleName: string): Parser {
+  const parserModule = require(moduleName);
+
+  if (typeof parserModule === 'function') return parserModule;
+  if (typeof parserModule.default === 'function') return parserModule.default;
+
+  throw new Error(`${moduleName} is not a valid parser`);
+}
+
+function getGenerators(
+  moduleNames: (string | GeneratorOptions)[],
+): Generator[] {
+  return moduleNames.map((x) => {
+    const moduleName = typeof x === 'string' ? x : x.generator;
+    const parserModule = require(moduleName);
+
+    if (typeof parserModule === 'function') return parserModule;
+    if (typeof parserModule.default === 'function') return parserModule.default;
+
+    throw new Error(`${moduleName} is not a valid parser`);
+  });
+}
+
+function* getFiles(service: Service, generators: Generator[]): Iterable<File> {
+  for (const generator of generators) {
+    yield* generator(service);
+  }
+}
+
+async function readStreamToString(stream: NodeJS.ReadStream) {
+  const chunks: any[] = [];
+  for await (const chunk of stream) chunks.push(chunk);
+  return Buffer.concat(chunks).toString('utf8');
+}
---
+++ src/types.ts
@@ -0,0 +1,226 @@
+export type Config = {
+  parser: string;
+  generators: (string | GeneratorOptions)[];
+  source?: string;
+  output?: string;
+};
+
+export type GeneratorOptions = {
+  generator: string;
+};
+
+export type Parser = (input: string) => Service;
+export type Generator = (service: Service) => File[];
+
+export interface ServiceFactory {
+  parse(): Service;
+}
+
+export interface FileFactory {
+  get target(): string;
+  build(service: Service): File[];
+}
+
+export type File = {
+  path: string[];
+  contents: string;
+};
+
+export type Service = {
+  title: string;
+  majorVersion: number;
+  interfaces: Interface[];
+  types: Type[];
+  enums: Enum[];
+};
+
+export type Type = {
+  name: string;
+  description?: string | string[];
+  properties: Property[];
+  rules: ObjectValidationRule[];
+};
+
+export type Enum = {
+  name: string;
+  values: string[];
+};
+
+export type Property = {
+  name: string;
+  description?: string | string[];
+  typeName: string;
+  isUnknown: boolean;
+  isArray: boolean;
+  isLocal: boolean;
+  rules: ValidationRule[];
+};
+
+export type Interface = {
+  name: string;
+  description?: string | string[];
+  methods: Method[];
+  protocols: {
+    http: PathSpec[];
+  };
+};
+
+export type PathSpec = {
+  path: string;
+  methods: MethodSpec[];
+};
+
+export type MethodSpec = {
+  name: string;
+  verb: string;
+  parameters: ParameterSpec[];
+  successCode: number;
+};
+
+export type ParameterSpec = {
+  name: string;
+  in: 'header' | 'query' | 'path' | 'formData' | 'body';
+  array?: 'csv' | 'ssv' | 'tsv' | 'pipes' | 'multi';
+};
+
+export type Method = {
+  name: string;
+  description?: string | string[];
+  parameters: Parameter[];
+  returnType: ReturnType | undefined;
+};
+
+export type Parameter = {
+  name: string;
+  description?: string | string[];
+  typeName: string;
+  isUnknown: boolean;
+  isArray: boolean;
+  isLocal: boolean;
+  rules: ValidationRule[];
+};
+
+export type ReturnType = {
+  typeName: string;
+  isUnknown: boolean;
+  isArray: boolean;
+  isLocal: boolean;
+  rules: ValidationRule[];
+};
+
+export type RequiredRule = {
+  id: 'required';
+};
+
+export type StringRule = {
+  id: 'string';
+};
+
+export type StringMaxLengthRule = {
+  id: 'string-max-length';
+  length: number;
+};
+
+export type StringMinLengthRule = {
+  id: 'string-min-length';
+  length: number;
+};
+
+export type StringPatternRule = {
+  id: 'string-pattern';
+  pattern: string;
+};
+
+export type StringFormatRule = {
+  id: 'string-format';
+  format: string;
+};
+
+export type StringEnumRule = {
+  id: 'string-enum';
+  values: string[];
+};
+
+export type NumberMultipleOfRule = {
+  id: 'number-multiple-of';
+  value: number;
+};
+
+export type NumberGtRule = {
+  id: 'number-gt';
+  value: number;
+};
+
+export type NumberGteRule = {
+  id: 'number-gte';
+  value: number;
+};
+
+export type NumberLtRule = {
+  id: 'number-lt';
+  value: number;
+};
+
+export type NumberLteRule = {
+  id: 'number-lte';
+  value: number;
+};
+
+export type ArrayMaxItemsRule = {
+  id: 'array-max-items';
+  max: number;
+};
+
+export type ArrayMinItemsRule = {
+  id: 'array-min-items';
+  min: number;
+};
+
+export type ArrayUniqueItemsRule = {
+  id: 'array-unique-items';
+  required: boolean;
+};
+
+export type ObjectMinPropertiesRule = {
+  id: 'object-min-properties';
+  min: number;
+};
+
+export type ObjectMaxPropertiesRule = {
+  id: 'object-max-properties';
+  max: number;
+};
+
+export type ObjectAdditionalPropertiesRule = {
+  id: 'object-additional-properties';
+  forbidden: true;
+};
+
+export type ValidationRule =
+  | RequiredRule
+  | StringMaxLengthRule
+  | StringMinLengthRule
+  | StringPatternRule
+  | StringFormatRule
+  | StringEnumRule
+  | NumberMultipleOfRule
+  | NumberGtRule
+  | NumberGteRule
+  | NumberLtRule
+  | NumberLteRule
+  | ArrayMaxItemsRule
+  | ArrayMinItemsRule
+  | ArrayUniqueItemsRule;
+
+export type ObjectValidationRule =
+  | ObjectMinPropertiesRule
+  | ObjectMaxPropertiesRule
+  | ObjectAdditionalPropertiesRule;
+
+export function isRequired(obj: { rules: ValidationRule[] }): boolean {
+  return obj.rules.some((r) => r.id === 'required');
+}
+
+export function isEnum(obj: { rules: ValidationRule[] }): boolean {
+  return obj.rules.some((r) => r.id === 'string-enum');
+}
