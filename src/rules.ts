8232e4a938effeaf862317e050d37c3230643022
Steve Konves <stephen.konves@gmail.com>
2022-05-31 21:14:14 -0700

    feat: add more rule helpers

--- src/rules.ts
+++ src/rules.ts
@@ -14,6 +14,12 @@ import {
   Violation,
 } from './types';
 
+export type ContextIterator<Context> = (
+  service: Service,
+  sourcePath: string,
+  options: any,
+) => Context[];
+
 export interface ServiceRuleContext {
   service: Service;
   sourcePath: string;
@@ -35,10 +41,21 @@ export function interfaceRule(
   rule: (context: InterfaceRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.interfaces
-      .map((i) => rule({ interface: i, service, sourcePath, options }))
+    allInterfaces(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allInterfaces: ContextIterator<InterfaceRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) =>
+  service.interfaces.map((i) => ({
+    interface: i,
+    service,
+    sourcePath,
+    options,
+  }));
 
 export interface MethodRuleContext extends InterfaceRuleContext {
   method: Method;
@@ -48,20 +65,25 @@ export function methodRule(
   rule: (context: MethodRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.interfaces
-      .flatMap((i) => i.methods.map<[Method, Interface]>((m) => [m, i]))
-      .map(([m, i]) =>
-        rule({
-          method: m,
-          httpMethod: getHttpMethodByName(service, m.name.value),
-          interface: i,
-          service,
-          sourcePath,
-          options,
-        }),
-      )
+    allMethods(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allMethods: ContextIterator<MethodRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) =>
+  service.interfaces
+    .flatMap((i) => i.methods.map<[Method, Interface]>((m) => [m, i]))
+    .map(([m, i]) => ({
+      method: m,
+      httpMethod: getHttpMethodByName(service, m.name.value),
+      interface: i,
+      service,
+      sourcePath,
+      options,
+    }));
 
 export interface HttpPathRuleContext extends InterfaceRuleContext {
   httpPath: PathSpec;
@@ -70,15 +92,24 @@ export function httpPathRule(
   rule: (context: HttpPathRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.interfaces
-      .flatMap((i) =>
-        i.protocols.http.map<[PathSpec, Interface]>((p) => [p, i]),
-      )
-      .map(([p, i]) =>
-        rule({ httpPath: p, interface: i, service, sourcePath, options }),
-      )
+    allHttpPaths(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allHttpPaths: ContextIterator<HttpPathRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) =>
+  service.interfaces
+    .flatMap((i) => i.protocols.http.map<[PathSpec, Interface]>((p) => [p, i]))
+    .map(([p, i]) => ({
+      httpPath: p,
+      interface: i,
+      service,
+      sourcePath,
+      options,
+    }));
 
 export interface ParameterRuleContext extends MethodRuleContext {
   parameter: Parameter;
@@ -88,28 +119,33 @@ export function parameterRule(
   rule: (context: ParameterRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.interfaces
-      .flatMap((i) => i.methods.map<[Method, Interface]>((m) => [m, i]))
-      .flatMap(([m, i]) =>
-        m.parameters.map<[Parameter, Method, Interface]>((p) => [p, m, i]),
-      )
-      .map(([p, m, i]) =>
-        rule({
-          parameter: p,
-          httpParameter: getHttpMethodByName(
-            service,
-            m.name.value,
-          )?.parameters?.find((x) => x.name.value === p.name.value),
-          method: m,
-          httpMethod: getHttpMethodByName(service, m.name.value),
-          interface: i,
-          service,
-          sourcePath,
-          options,
-        }),
-      )
+    allParameters(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allParameters: ContextIterator<ParameterRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) =>
+  service.interfaces
+    .flatMap((i) => i.methods.map<[Method, Interface]>((m) => [m, i]))
+    .flatMap(([m, i]) =>
+      m.parameters.map<[Parameter, Method, Interface]>((p) => [p, m, i]),
+    )
+    .map(([p, m, i]) => ({
+      parameter: p,
+      httpParameter: getHttpMethodByName(
+        service,
+        m.name.value,
+      )?.parameters?.find((x) => x.name.value === p.name.value),
+      method: m,
+      httpMethod: getHttpMethodByName(service, m.name.value),
+      interface: i,
+      service,
+      sourcePath,
+      options,
+    }));
 
 export interface TypeRuleContext extends ServiceRuleContext {
   type: Type;
@@ -118,10 +154,15 @@ export function typeRule(
   rule: (context: TypeRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.types
-      .map((type) => rule({ type, service, sourcePath, options }))
+    allTypes(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allTypes: ContextIterator<TypeRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) => service.types.map((type) => ({ type, service, sourcePath, options }));
 
 export interface PropertyRuleContext extends TypeRuleContext {
   property: Property;
@@ -130,13 +171,18 @@ export function propertyRule(
   rule: (context: PropertyRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.types
-      .flatMap((t) => t.properties.map<[Property, Type]>((p) => [p, t]))
-      .map(([p, t]) =>
-        rule({ property: p, type: t, service, sourcePath, options }),
-      )
+    allProperties(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allProperties: ContextIterator<PropertyRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) =>
+  service.types
+    .flatMap((t) => t.properties.map<[Property, Type]>((p) => [p, t]))
+    .map(([p, t]) => ({ property: p, type: t, service, sourcePath, options }));
 
 export interface EnumRuleContext extends ServiceRuleContext {
   enum: Enum;
@@ -145,10 +191,15 @@ export function enumRule(
   rule: (context: EnumRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.enums
-      .map((e) => rule({ enum: e, service, sourcePath, options }))
+    allEnums(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allEnums: ContextIterator<EnumRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) => service.enums.map((e) => ({ enum: e, service, sourcePath, options }));
 
 export interface EnumValueRuleContext extends EnumRuleContext {
   value: Enum['values'][number];
@@ -157,15 +208,18 @@ export function enumValueRule(
   rule: (context: EnumValueRuleContext) => Violation | undefined,
 ): Rule {
   return (service, sourcePath, options) =>
-    service.enums
-      .flatMap((e) =>
-        e.values.map<[Enum['values'][number], Enum]>((v) => [v, e]),
-      )
-      .map(([v, e]) =>
-        rule({ value: v, enum: e, service, sourcePath, options }),
-      )
+    allEnumValues(service, sourcePath, options)
+      .map((context) => rule(context))
       .filter((v): v is Violation => !!v);
 }
+export const allEnumValues: ContextIterator<EnumValueRuleContext> = (
+  service,
+  sourcePath,
+  options,
+) =>
+  service.enums
+    .flatMap((e) => e.values.map<[Enum['values'][number], Enum]>((v) => [v, e]))
+    .map(([v, e]) => ({ value: v, enum: e, service, sourcePath, options }));
 
 export function combineRules(...rules: Rule[]): Rule {
   return (service, sourcePath, options) =>
